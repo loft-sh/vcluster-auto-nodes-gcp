3953143190840da01c1c748925b6b11113d33f9d
Jan Baraniewski <dev@baraniewski.com>
2025-10-09 18:11:33 +0200

    Add nodeselector matching provider nodes to ccm deployment (#3)
    * Add nodeselector matching provider nodes to ccm deployment
    
    * Add var to csi too

--- environment/kubernetes/main.tf
+++ environment/kubernetes/main.tf
@@ -1,5 +1,5 @@
 ##########
-# CCM 
+# CCM
 #########
 
 module "kubernetes_apply_ccm" {
@@ -10,6 +10,7 @@ module "kubernetes_apply_ccm" {
     network_name  = local.network_name
     subnet_name   = local.subnet_name
     vcluster_name = local.vcluster_name
+    node_provider_name = local.node_provider_name
   }
 }
 
@@ -23,4 +24,8 @@ module "kubernetes_apply_csi" {
   # The oldest supported k8s versio is 1.30.x, that requires CSI Driver 1.13.x
   manifest_file = "${path.module}/manifests/csi.yaml.tftpl"
   computed_fields = ["globalDefault"]
+
+  template_vars = {
+    node_provider_name = local.node_provider_name
+  }
 }
