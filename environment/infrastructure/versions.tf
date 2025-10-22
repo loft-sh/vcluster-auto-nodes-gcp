7a2135ecdf133af455800238fd4c5b6415bba55b
Marcin Franczyk <marcin0franczyk@gmail.com>
2025-10-22 11:47:04 +0200

    fix deps lock
    Signed-off-by: Marcin Franczyk <marcin0franczyk@gmail.com>

--- environment/infrastructure/versions.tf
+++ environment/infrastructure/versions.tf
@@ -3,7 +3,20 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 7.7.0"
+      version = "~> 6.50.0"
+    }
+    # TODO: NAT module uses google-beta, we should drop it
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 6.50.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7.2"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2.4"
     }
   }
 }
